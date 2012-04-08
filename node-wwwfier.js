be8a1bff042983aedf27f963ebcd7fcb06319026
Richard Harvey <richard@squarecows.com>
2012-04-08 22:10:11 +0100

    Open Sourcing redirect project

---
+++ node-wwwfier.js
@@ -0,0 +1,26 @@
+var cluster = require('cluster');
+var express = require('express');
+var http = express.createServer();
+
+http.get('/*', function(req, res, next) {
+
+    if (req.headers.host.match(/^www/) == null ) res.redirect('http://www.' + req.headers.host + req.url, 301);
+    
+    else res.send('URL Not Supported please refer to the documentation', { 'Content-Type': 'text/plain' }, 400);
+
+});
+
+var numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('death', function(worker) {
+    console.log('worker ' + worker.pid + ' died');
+  });
+} else {
+    // Worker processes have a http server.
+    http.listen(3000);
+}
