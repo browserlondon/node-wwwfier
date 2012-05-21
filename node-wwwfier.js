ac98fb85945d4005b5320c99ad5aa6219834c0ea
Richard Harvey <richard@squarecows.com>
2012-05-21 15:33:27 +0000

    Added support to strip www from URL

--- node-wwwfier.js
+++ node-wwwfier.js
@@ -15,9 +15,17 @@ if (cluster.isMaster) {
     // Worker processes have a http server.
     http.get('/*', function(req, res, next) {
 
-        if (req.headers.host.match(/^www/) == null ) res.redirect('http://www.' + req.headers.host + req.url, 301);
-
-        else res.send('URL Not Supported please refer to the documentation', { 'Content-Type': 'text/plain' }, 400);
+        if (req.headers.host.match(/^www/) == null ) {
+		res.redirect('http://www.' + req.headers.host + req.url, 301);
+	}
+	else if (req.headers.host.match(/^www/) != null ) {
+		var host = req.headers.host;
+		host = host.replace(/^www\.(.+\.)/, '$1');
+		res.redirect('http://' + host + req.url, 301);
+	}
+        else {
+		res.send('Ooops Theres been a server error', { 'Content-Type': 'text/plain' }, 500);
+	}
 
     });
     http.listen(3000);
